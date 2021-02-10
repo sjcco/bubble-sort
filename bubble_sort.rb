90400ac2fc14c74fbdb05a9d1004162f4992038a
sjcco <73748496+sjcco@users.noreply.github.com>
2021-02-10 09:35:02 -0600

    fixed linter error

---
+++ bubble_sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort(array)
+  sorting = true    
+  while sorting do
+    sorting = false
+    i = 0
+    array.length.times do
+      if array[i] > array[i+1]
+        array[i+1], array[i] = array[i], array[i +1]
+        sorting = true
+      end
+      i+=1
+      break if array.length-1 == i
+    end
+  end
+  p array
+end
+bubble_sort([5,1,3,2,4,9,7,6,8,-11])
