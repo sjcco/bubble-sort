4df163b48e5ff077f406ef62ffe60b44debe86d3
sjcco <73748496+sjcco@users.noreply.github.com>
2021-02-10 11:30:09 -0600

    fixed linter error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -16,14 +16,20 @@ def bubble_sort(array)
 end
 
 def bubble_sort_by(array)
-  for i in 0..(array.length - 2) do
+  i = 0
+  while i < array.length - 1
     is_bigger = yield(array[i], array[i + 1]).positive?
-    array[i + 1], array[i] = array[i], array[i + 1] if is_bigger
+    if is_bigger
+      array[i + 1], array[i] = array[i], array[i + 1]
+      i = 0
+    else
+      i += 1
+    end
   end
   p array
 end
 
 bubble_sort([5, 1, 3, 2, 4, 9, 7, 6, 8, -11])
-bubble_sort_by(%w[hi hello hey]) do |left, right|
+bubble_sort_by(%w[hi hello hey asdf asdfa]) do |left, right|
   left.length - right.length
 end
